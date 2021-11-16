a233706f342e0e6040050c652f4bb692d0523b63
Erik <erikroystan@gmail.com>
2021-11-16 11:13:43 -0800

    Update namespaces from FixedPoint to Deterministic.FixedPoint, matching updates to the source library.

--- Editor/fpDrawer.cs
+++ Editor/fpDrawer.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 
-namespace FixedPoint
+namespace Deterministic.FixedPoint
 {
     [CustomPropertyDrawer(typeof(fp))]
     public class fpDrawer : PropertyDrawer
